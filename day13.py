d4cfb0018ac17111b1209c2f03471b3549e932e4
Peter Szucs <szpetyus@gmail.com>
2024-12-13 20:27:18 +0100

    day13 part 2 refactored

--- day13.py
+++ day13.py
@@ -1,7 +1,3 @@
-import math
-
-claws = []
-
 class Cclaw:
     def __init__(self):
         self.ax = 0
@@ -15,10 +11,11 @@ class Cclaw:
         return f"claw({self.px} {self.py})"
 
 
-with open("input13a.txt") as ip:
+claws = []
+with open("input13.txt") as ip:
     claw = Cclaw()
     for x in ip:
-        x = x.strip().replace(',', '').replace('Button ', '').replace(':', '').replace('X', '')\
+        x = x.strip().replace(',', '').replace('Button ', '').replace(':', '').replace('X', '') \
             .replace('Y', '').replace('=', '').split(' ')
         if len(x) == 1:
             claw = Cclaw()
@@ -33,40 +30,23 @@ with open("input13a.txt") as ip:
             claw.py = int(x[2])
             claws.append(claw)
 
-print(claws[:3])
-def price(x,y):
-    return x*3+y
 
-assert price(38,86) == 200
+def price(x3, y1):
+    return x3 * 3 + y1
 
-part1 = 0
-for claw in claws:
-    op = []
-    for a in range(claw.px//claw.ax+1):
-        for b in range(claw.px//claw.bx+1):
-            if a*claw.ax + b*claw.bx == claw.px:
-                print(a, b, claw.px, claw.ax, claw.bx, claw.ax / claw.bx, claw.bx / claw.ax, a/b, b/a, math.gcd(claw.px, claw.ax), math.gcd(claw.px, claw.bx), math.gcd(claw.px, claw.ax * claw.bx))
-                if a*claw.ay + b*claw.by == claw.py:
-                    op.append((price(a, b), a, b))
-    op = sorted(op)
-    print(sorted(op))
-    if len(op)>0:
-        part1 += op[0][0]
-print("part 1", part1)
 
-EC = 10000000000000
-part2 = 0
-for claw in claws:
-    op = []
-    print(0, (claw.px)//claw.ax, (claw.px) % claw.ax, math())
-    for a in range(0, (EC+claw.px)//claw.ax+1, (EC+claw.px) % claw.ax):
-        for b in range(0, (EC+claw.px)//claw.bx+1, (EC+claw.px) % claw.bx):
-            if a*claw.ax + b*claw.bx == EC+claw.px:
-                if a*claw.ay + b*claw.by == EC+claw.py:
-                    op.append((price(a, b), a, b))
-    op = sorted(op)
-    print(sorted(op))
-    if len(op)>0:
-        part2 += op[0][0]
-print("part 2", part2)
+assert price(38, 86) == 200
 
+for p, EC in enumerate([0, 10000000000000]):
+    part = 0
+    for claw in claws:
+        op = []
+        py = EC + claw.py
+        px = EC + claw.px
+
+        a = (py * claw.bx - px * claw.by) / (claw.bx * claw.ay - claw.ax * claw.by)
+        b = (px - a * claw.ax) / claw.bx
+        if a == int(a) and b == int(b) and a > 0 and b > 0:
+            part += price(a, b)
+
+    print(f"part {p + 1}: {int(part)}")
